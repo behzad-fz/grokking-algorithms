9e8d20daa2c01a1f8b4fa5bc35a5b1cec71dde9e
BehzadFazelasl <behzadfazelasl@teknasyon.com>
2022-04-27 11:47:33 +0300

    Add simple search logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use std::any::type_name;
+
+fn type_of<T>(_: T) -> &'static str {
+    type_name::<T>()
+}
+
 fn main() {
-    println!("Binary Search!");
+    println!("Simple Search!");
+
+    // create haystack
+    let mut haystack: [u8; 100] = [0; 100];
+
+    // initiate haystack
+    for number in 0u8..100 {
+        haystack[number as usize] = number + 1;
+    }
+
+    // get needle from user
+    let needle: u8 = 50;
+
+    // index for walking on array
+    let mut index:usize = 0;
+
+    loop {
+        println!("Step {}", index + 1);
+        
+        println!("Is it {} ?", haystack[index]);
+
+        if haystack[index] == needle {
+            println!("Hooray");
+            break;
+        }   
+
+        index = index + 1;
+    }
+
+    println!("We found it in {} steps!", index + 1);
 }
